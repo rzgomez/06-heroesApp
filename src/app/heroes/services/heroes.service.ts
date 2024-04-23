dae4642e6dbeb994b10f0fba9f796f5b5de122b8
Rossana Chi <amyeluzr@gmail.com>
2024-04-23 12:33:41 -0600

    Fin de la sección 15

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -2,7 +2,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { enviroments } from 'src/enviroments/enviroments';
 
@@ -28,6 +28,28 @@ export class HeroesService {
   getSuggestions( query: string ): Observable<Hero[]> {
     console.log(query);
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&attr=superhero&_limit=6`);
+  }
+
+
+  addHero( hero: Hero ): Observable<Hero> {
+    return this.http.post<Hero>(`${ this.baseUrl }/heroes`, hero);
+  }
+
+
+  updateHero( hero: Hero ): Observable<Hero> {
+    if( !hero.id ) throw Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${ this.baseUrl }/heroes/${ hero.id }`, hero);
+  }
+
+
+  deleteHeroById( id: string ): Observable<boolean> {
+
+    return this.http.delete(`${ this.baseUrl }/heroes/${ id }`)
+    .pipe(
+      map( resp => true ),
+      catchError( err => of( false )),
+    );
   }
 
 }
